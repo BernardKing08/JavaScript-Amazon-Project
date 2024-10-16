0204190455847e272d40e7dc025144b4ef526ee2
BernardKing08 <bernardkingsley08@gmail.com>
2024-10-16 11:49:51 +0100

    Updating the cart product quantity with the new quantity

--- data/cart.js
+++ data/cart.js
@@ -86,3 +86,17 @@ export function updateDeliveryOption(productId, deliveryOptionId){
 
     saveToStorage();
 }
+
+export function updateQuantity(productId, newQuantity){
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+    
+      matchingItem.quantity = newQuantity;
+    
+      saveToStorage();
+}
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js'
+import {cart, removeFromCart, updateDeliveryOption, updateQuantity} from '../../data/cart.js'
 import { products, getProduct } from '../../data/products.js';
 import formatCurrency from '../utlis/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -57,11 +57,11 @@ export function renderOrderSummary(){
                 <span>
                   Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                 </span>
-                <span class="update-quantity-link link-primary js-update-link" data-product-id=${matchingProduct.id}>
+                <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
                   Update 
                 </span>
-                <input class="quantity-input">
-                <span class="save-quantity-link link-primary">Save</span>
+                <input class="quantity-input js-quantity-input-${matchingProduct.id}">
+                <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">Save</span>
                 <span class="delete-quantity-link link-primary js-delete-link" data-product-id = '${matchingProduct.id}'>
                   Delete
                 </span>
@@ -154,6 +154,23 @@ document.querySelectorAll('.js-update-link').forEach(link => {
 	})
 })
 
+document.querySelectorAll('.js-save-link').forEach(link => {
+	link.addEventListener('click', () => {
+		const productId = link.dataset.productId;
+
+		const container = document.querySelector(
+			`.js-cart-item-container-${productId}`
+		);
+		container.classList.remove('is-editing-quantity');
+
+		const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
+		const newQuantity = Number(quantityInput.value);
+
+		//updating the quantity with the new quantity
+		updateQuantity(productId, newQuantity);
+	})
+})
+
 document.querySelectorAll('.js-delivery-option').forEach((element) => {
 	element.addEventListener('click', () => {
 		const {productId, deliveryOptionId} = element.dataset
@@ -163,5 +180,4 @@ document.querySelectorAll('.js-delivery-option').forEach((element) => {
 	})
   })
 }
-
 
