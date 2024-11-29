d6453870f5cd2c4dd8707f65180ccc5e4699005c
BernardKing08 <bernardkingsley08@gmail.com>
2024-11-29 23:51:09 +0100

    Create OOp version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCartQuantity } from "./checkout/cartQuantity.js";
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 
