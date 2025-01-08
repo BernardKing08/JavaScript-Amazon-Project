d69d8c9972ac2d8c99bfed72b734160742100c7e
BernardKing08 <bernardkingsley08@gmail.com>
2025-01-08 14:58:48 +0100

    Learned Promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,11 +3,20 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCartQuantity } from "./checkout/cartQuantity.js";
 import { loadProducts } from "../data/products.js";
 
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}).then(() =>{
+    renderOrderSummary();
+    renderPaymentSummary();    
+    renderCartQuantity();
+})
+
+/*
 loadProducts(() => {
     renderOrderSummary();
-
-    renderPaymentSummary();
-    
+    renderPaymentSummary();    
     renderCartQuantity();
 });
-
+*/
